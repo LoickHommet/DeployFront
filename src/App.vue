9ac4bb7514925432491910576b6404b1c73fe120
Loick Hommet <hommet.loick@gmail.com>
2022-06-21 09:47:39 +0200

    DeployFront

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import navBar from "@/components/navBar.vue"
+import {ref} from "vue"
+import { useUserStore } from './services/userstore';
+
+    const {monCompte} = useUserStore();
+    monCompte();
+
+
+</script>
+
+<template>
+<div>
+<navBar></navBar>
+<RouterView></RouterView>
+
+</div>
+
+</template>
+
+<style >
+@import 'bootstrap/dist/css/bootstrap.min.css';
+</style>
